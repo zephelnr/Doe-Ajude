f9805fbc3077c37dc9c35a774f8f5ddca20d8cdd
JosephSAlexandre <160176206+JosephSAlexandre@users.noreply.github.com>
2024-09-30 17:07:42 -0300

    testeConexaoComentado

--- src/conexao.php
+++ src/conexao.php
@@ -6,4 +6,14 @@ const BANCO = "mydb";
 const USUARIO = "root";
 
 const SENHA = "";
+/*
+$DSN = "mysql:dbname=mydb;host=localhost";
+
+try{
+    $pdo = new PDO($DSN, USUARIO, SENHA);
+    echo "Sucesso na conexão";
+} catch(PDOException $e){
+    echo "Falha na conexão com o banco de dados: " . $e->getMessage();
+    die();
+}*/
 ?>
