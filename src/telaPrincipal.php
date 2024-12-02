b3df7342231a5e1b06173bdc86d76726d781b9fa
JosephSAlexandre <160176206+JosephSAlexandre@users.noreply.github.com>
2024-12-02 16:46:07 -0300

    AlteraçõesPI2

--- src/telaPrincipal.php
+++ src/telaPrincipal.php
@@ -36,6 +36,35 @@ if (empty($_SESSION['email'])) {
 
     <body>
         <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-success-subtle">
+                <div class="container-fluid">
+
+                    <a class="navbar-brand" href="telaPrincipal.php">Doe&Ajude</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="perfil.php">Perfil</a>
+                            <a class="nav-link" href="publicacoes.php">Publicações</a>
+                            <a class="nav-link disabled" aria-disabled="true">Interesses</a>
+                            <a class="nav-link disabled" aria-disabled="true">Interessados</a>
+                        </div>
+                    </div>
+                   
+                    <!--<div class="d-grid gap-5 d-md-flex justify-content-md-center">
+                        <a class="navbar-brand" href="telaPrincipal.php">Doe&Ajude</a>
+                        <a class="btn btn-success rounded-pill" href="perfil.php" role="button">Perfil</a>
+                        <a class="btn btn-success rounded-pill" href="publicacoes.php" role="button">Publicações</a>
+                        <a class="btn btn-success rounded-pill" href="interesse.php" role="button">Interesses</a>
+                        <a class="btn btn-success rounded-pill" href="interessado.php" role="button">Interessados</a>
+                    </div>
+                    <a class="btn btn-success rounded-pill" href="logout.php" role="button">Sair</a>-->
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                        <a class="btn btn-success rounded-pill" href="logout.php" role="button">Sair</a>
+                    </div>
+                </div>
+            </nav>
             <!-- place navbar here -->
             <nav class="navbar bg-body-tertiary bg-success-subtle">
                 <div class="container-fluid">
@@ -68,14 +97,12 @@ if (empty($_SESSION['email'])) {
                      -->
                     
                      <form class="container-fluid">
-                        <div class="input-group">
-                            <a class="navbar-brand" href="telaPrincipal.php">Doe&Ajude</a>
-                            <form action="">
+                        <div class="input-group gap-2">
+                            <a class="btn btn-success rounded-pill" href="cadastrarPublicacao.php" role="button">Cadastrar Nova Publicação</a>
+                            <form action=""> 
                                 <input type="search" class="form-control rounded-pill" placeholder="Buscar" aria-label="Busca" aria-describedby=""> 
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <div class="d-grid d-md-flex justify-content-md-center">
                                     <button class="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
-                                    <a class="btn btn-success rounded-pill" href="perfil.php" role="button">Menu</a>
-                                    <a class="btn btn-success rounded-pill" href="logout.php" role="button">Sair</a>
                                 </div>
                             </form>
                         </div>
