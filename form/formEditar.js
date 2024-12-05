1f9d2c01f44499b9a2ff37d32ff1afeeacc1be8f
JosephSAlexandre <160176206+JosephSAlexandre@users.noreply.github.com>
2024-12-05 19:53:10 -0300

    formEditar

---
+++ form/formEditar.js
@@ -0,0 +1,28 @@
+const btnEditar = document.getElementById("btnEditar");
+btnEditar.addEventListener("click", (e) => {
+   const frmEditar = document.getElementById("frmEditar");
+   let formData = new FormData(frmEditar);
+   
+   // as 2 linhas abaixo são no caso de uma alterção ou exclusão
+   let jsonData = JSON.stringify(Object.fromEntries(formData));
+   console.log(jsonData);
+
+   let xhr = new XMLHttpRequest();
+   xhr.onload = function() {
+      if (xhr.status==200 && xhr.readyState==4) {
+         console.log(xhr.responseText);
+      }
+      else {
+         console.log("XMLHttpRequest Error");
+      }
+   }
+   // troque o DELETE abaixo pelo método que vc precisa
+   // GET, POST, PUT, DELETE
+   // lembrando que no caso de consultas (GET), vc deve enviar os parâmetros diretamente no URL
+   xhr.open("PUT","form.php");
+   // se for inclusão(POST) vc envia o formData
+   // se for alteração ou exclusão (PUT ou DELETE) você envia o jsonData
+   // se for uma consulta (GET), vc deve enviar os dados pelo URL, como mencionei acima
+   xhr.send(jsonData);
+})
+
