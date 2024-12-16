0faf57634ef140fc1c620d0352f7943d9aeac9c0
JosephSAlexandre <160176206+JosephSAlexandre@users.noreply.github.com>
2024-12-16 15:31:18 -0300

    index_getRetiradaBotao

--- src/index_get.php
+++ src/index_get.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD']=='GET') {
                             
                             echo "<div class='d-flex justify-content-between align-items-center'>";
                                 echo "<div class='btn-group'>";
-                                    echo "<button type='button' class='btn btn-sm btn-outline-success'>Visualizar</button>";
+                                    //echo "<button type='button' class='btn btn-sm btn-outline-success'>Visualizar</button>";
                                     //echo "<button type='button' class='btn btn-sm btn-outline-success'>Editar</button>";
                                 echo "</div>";
                                 echo "<small class='text-muted'>" . $row['cidade'] . "</br>" . $row['estado'] . "</br>Data de publicação: " .  $dataFormatada . "</small>";
