b9211de93c681e84a4d4b4d34270140206121d6e
JosephSAlexandre <160176206+JosephSAlexandre@users.noreply.github.com>
2025-01-29 23:31:37 -0300

    visualizacaoDetalhadaFuncional

---
+++ src/js/visualizacaoDetalhada.js
@@ -0,0 +1,74 @@
+document.getElementById("email").textContent = email;
+const id_publicacao = document.getElementById("idPublicacao").value;
+//console.log("email",email.value);
+//console.log("id",id_publicacao);
+function carregarDetalhes() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET","visualizacaoDetalhada_get.php?id_publicacao=" + id_publicacao + "&email=" + email.value);
+    xhr.onreadystatechange = function() {
+        if (xhr.status==200 && xhr.readyState==4) {
+            console.log(xhr.responseText);
+
+            const titulo = document.getElementById("titulo");
+            const descricao = document.getElementById("descricao");
+            const cidade_estado = document.getElementById("cidadeEstado");
+            const status_data = document.getElementById("statusData");
+
+            var response = xhr.responseText;
+            
+
+            // Criando um elemento temporário para analisar o HTML
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(response, "text/html");
+            // Pegando o conteúdo de um elemento específico
+            var tituloGet = doc.querySelector("h2").textContent;
+            var descricaoGet = doc.querySelector("h3").textContent;
+            var cidadeGet = doc.querySelector("h4").textContent;
+            var estadoGet = doc.querySelector("h5").textContent;
+            var statusGet = doc.querySelector("h6").textContent;
+            var dataGet = doc.querySelector("h7").textContent;
+
+            //formatar data
+            const data = new Date(dataGet);
+            const dataFormatada = data.toLocaleDateString('pt-BR'); // Formato: dd/mm/aaaa
+            console.log(dataFormatada);
+
+            // Exibindo os dados
+            titulo.value = tituloGet;
+            descricao.value = descricaoGet;
+            cidade_estado.value = cidadeGet + " / " + estadoGet;
+            status_data.value = statusGet + " em: " + dataFormatada;
+
+        }
+        else {
+            //console.log("XMLHttpRequest Error");
+        }
+    }
+    xhr.send();
+
+}
+
+window.onload = carregarDetalhes;
+
+
+/*
+function carregarDadosSessao() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","perfil.php?", true); // Requisição para o PHP que retorna a sessão
+    xhr.onreadystatechange = function() {
+        if (xhr.status==200 && xhr.readyState==4) {
+            var resposta = JSON.parse(xhr.responseText);
+            
+            if (resposta.usuario) {
+                document.getElementById("usuario").textContent = resposta.usuario;
+                //document.getElementById("email").textContent = resposta.email;
+                //document.getElementById("idade").textContent = resposta.idade;
+            } else {
+                console.log(resposta.error);
+            }
+        }
+    };
+    xhr.send();
+}
+
+window.onload = carregarDadosSessao; // Chama a função quando a página carrega*/
---
+++ src/visualizacaoDetalhada_get.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+if (empty($_SESSION['email'])) {
+    header("Location: index.php");
+}
+?>
+<?php
+require_once("conexao.php");
+if ($_SERVER['REQUEST_METHOD']=='GET') {
+    try {    
+        // Verificar se a conexão foi bem-sucedida
+        if ($conn->connect_error) {
+            die("Conexão falhou: " . $conn->connect_error);   
+        }
+    
+        // Verificar se o parâmetro 'email' foi passado via GET
+        if (isset($_GET['id_publicacao']) && isset($_GET['email'])) {
+            $id_publicacao = $_GET['id_publicacao'];
+            $email = $_GET['email'];
+            //print_r($_GET['email']);
+            //print_r($_GET['id_publicacao']);
+            
+            // Consultar o banco de dados para pegar os dados do usuário
+            $sql = "SELECT publicacao.titulo, publicacao.descricao, publicacao.foto, publicacao.status, publicacao.data, publicacao.usuario_email, municipio.nome AS cidade, estado.nome AS estado FROM publicacao INNER JOIN municipio ON publicacao.cidade = municipio.id_municipio INNER JOIN estado ON publicacao.estado = estado.id_estado WHERE id_publicacao = '$id_publicacao'";
+            $result = $conn->query($sql);
+    
+            // Verificar se o usuário existe
+            if ($result->num_rows > 0) {
+                // Converte e formata a data
+                //$data = new DateTime($row['data']);
+                //$dataFormatada = $data->format('d/m/Y');
+
+                // Exibir os dados do usuário
+                $row = $result->fetch_assoc();
+                echo "<h1>Informações da Publicação</h1>";
+                //echo "<p>email: " . $row['email'] . "<p><br>";
+                echo "<h2>" . $row['titulo'] ."</h2><br>";
+                echo "<h3>" . $row['descricao'] ."</h3><br>";
+                echo "<h4>" . $row['cidade'] . "</h4><br>";
+                echo "<h5>" . $row['estado'] . "</h5><br>";
+                echo "<h6>" . $row['status'] . "</h6><br>";
+                echo "<h7>" . $row['data'] . "</h7><br>";
+                echo "<h8>" . $row['usuario_email'] . "</h8><br>";
+                echo "<h9>" . $row['foto'] . "</h9><br>";
+            } else {
+                throw new Exception("Nenhuma publicação encontrada com esse id.");
+            }
+        } else {
+            //throw new Exception("Por favor, informe um email de usuário.");
+        }
+    
+        // Fechar a conexão com o banco de dados
+        $conn->close();
+    } catch (Exception $e) {
+        //throw $e;
+        echo json_encode(["Erro!" . $e->getMessage()]);
+        //print_r(["Erro!" . $e->getMessage()]);
+    } finally {
+        $conn = null;
+    }
+    
+ }
+?>
