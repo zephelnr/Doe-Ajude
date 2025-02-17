bf963fc61f4d14224a74e85941b38cd78233b189
zephelnr <josephsalexandre@gmail.com>
2025-02-17 16:17:42 -0300

    correcaoCaracteresCidade

--- src/conexao.php
+++ src/conexao.php
@@ -8,6 +8,7 @@ $dbname = "doeajudebd";
 try {
     // Cria a conexão
     $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8mb4");
 } catch (PDOException $e) {
     die(json_encode(["status" => "error", "message" => "Falha na conexão: " . $conn->connect_error]));
 }
