39ac669eeb0c7f2342a6dd9439664f1ae448c11b
JosephSAlexandre <josephsalexandre@gmail.com>
2025-02-21 14:14:56 -0300

    HomeLoginCadastro

--- src/cadastro.php
+++ src/cadastro.php
@@ -123,9 +123,19 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
     <body>
         <header>
             <!-- place navbar here -->
-            <nav class="navbar bg-body-tertiary bg-success-subtle">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary  bg-success-subtle">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.php">Doe&Ajude</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="telaPrincipal.php">Home</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
